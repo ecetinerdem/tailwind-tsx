43f509067e0d571850f1644aae10082b37a3e205
ecetinerdem <cetoerdem1@gmail.com>
2024-11-06 23:31:01 +0400

    Initial commit

---
+++ src/components/UserList.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react';
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string; // corrected 'phıne' to 'phone'
+}
+
+const UserList = () => {
+  const [users, setUsers] = useState<User[]>([]); // corrected 'USer' to 'User'
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(
+          'https://jsonplaceholder.typicode.com/users'
+        );
+        if (!response.ok) throw new Error('Network response was not ok!');
+        const data: User[] = await response.json();
+        setUsers(data);
+      } catch (error) {
+        setError(error instanceof Error ? error.message : 'An error occurred');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUsers(); // corrected 'fetchIsers' to 'fetchUsers'
+  }, []);
+
+  if (loading) return <h1>Loading</h1>;
+  if (error) return <h1>Error: {error}</h1>;
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Username</th>
+          <th>Email</th>
+          <th>Phone</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.username}</td>
+            <td>{user.email}</td>
+            <td>{user.phone}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default UserList;
